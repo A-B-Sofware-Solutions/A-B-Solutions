eb90e88163b700833c72a9276863e0ad71396898
bobikenobi12 <borislav1207@gmail.com>
2024-01-30 11:48:06 +0200

    replace icons and certain images

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,43 +3,37 @@ const services = [
 		title: "MVP development",
 		description:
 			"Develop an MVP to test and attract investors for your business idea, showcasing its potential in a concise, impactful manner.",
-		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+		icon: `material-symbols:lightbulb-outline-rounded`,
 	},
 	{
 		title: "Automation of internal processes",
 		description:
 			"Automate the internal processes of your business and increase its efficiency",
-		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+		icon: `carbon:reference-architecture`,
 	},
 	{
 		title: "Web and mobile app development",
 		description:
 			"Create a web or mobile app to attract new customers and make your service more convenient for them",
-		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+		icon: `fluent:view-desktop-mobile-20-regular`,
 	},
 	{
 		title: "Data Analytics and Business Intelligence",
 		description:
 			"Drive success with strategic insights. Our services span data analytics, business intelligence, and informed decision-making for clients.",
-		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+		icon: `tabler:device-analytics`,
 	},
 	{
 		title: "Cybersecurity Consulting",
 		description:
 			"Fortify digital assets with assessments, testing, and policy development. Our cybersecurity services protect businesses from evolving cyber threats.",
-		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+		icon: `ic:round-security`,
 	},
 	{
 		title: "Custom Software Integration",
 		description:
 			"Optimize operations by seamlessly integrating software systems. From CRM to ERP, our services ensure efficient data flow and processes.",
-		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+		icon: `gridicons:custom-post-type`,
 	},
 ];
 
